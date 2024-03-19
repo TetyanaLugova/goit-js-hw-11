e68889495ed3152c9fb32b4e66e119e24916a2fa
TetyanaLugova <lugovatanya1503@gmail.com>
2024-03-19 18:44:02 +0200

    fix1

--- src/main.js
+++ src/main.js
@@ -1,28 +1,24 @@
-//  
-import {formRef, inputRef, galleryRef, fetchImg} from "./js/pixabay-api";
-import { createMarkup } from "./js/render-functions";
-
+//
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
-let queryInput = "";
-
+import { formRef, inputRef, galleryRef, fetchImg } from "./js/pixabay-api";
+import { createMarkup } from "./js/render-functions";
+let queryInput = ''; // створюємо локальну змінну для зберігання значення запиту
 inputRef.addEventListener("input", (event) => {
-    queryInput = inputRef.value.trim();
+    queryInput = inputRef.value.trim(); // оновлюємо значення локальної змінної при введенні
     galleryRef.innerHTML = '';
 });
-
 formRef.addEventListener("submit", (event)=>{
-  event.preventDefault();
-  if (queryInput) {
-     galleryRef.innerHTML = '<div class="loader"></div>';
-    fetchImg(queryInput)
-    .then(data => createMarkup(data))
-    .catch(error => {
-      iziToast.error({
-        title: 'Error',
-        message: "Sorry, an error occurred while fetching images. Please try again later!",
-      });
-    });
-  }
+    event.preventDefault();
+    if (queryInput) {
+        galleryRef.innerHTML = '<div class="loader"></div>';
+        fetchImg(queryInput)
+        .then(data => createMarkup(data))
+        .catch(error => {
+            iziToast.error({
+                title: 'Error',
+                message: "Sorry, an error occurred while fetching images. Please try again later!",
+            });
+        });
+    }
 });
